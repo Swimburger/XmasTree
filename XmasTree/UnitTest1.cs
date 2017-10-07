1e639c7a4433c087200fd6a737ec36f196458a78
Niels Swimberghe <nielsswimberghe@gmail.com>
2017-10-07 19:00:56 +0200

    Add project files.

---
+++ XmasTree/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmasTree
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
